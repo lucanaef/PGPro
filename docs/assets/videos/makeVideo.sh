6c6b650a4bdb2a98294a8c4c2b718b383586eba3
Luca Näf <lucanaef@users.noreply.github.com>
2019-12-25 15:40:54 +0100

    make video slower

--- docs/assets/videos/makeVideo.sh
+++ docs/assets/videos/makeVideo.sh
@@ -7,7 +7,7 @@ rm *.webm
 cp ../screenshot/* .
 
 # create video (mp4 format)
-ffmpeg -framerate 0.5 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p screenshots.mp4
+ffmpeg -framerate 0.25 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p screenshots.mp4
 # get webm file
 ffmpeg -i screenshots.mp4 screenshots.webm
 
