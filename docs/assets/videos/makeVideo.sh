dc61f018db7c051229a538a491533575e9135294
Luca Näf <lucanaef@users.noreply.github.com>
2019-12-25 15:37:32 +0100

    add video to website

---
+++ docs/assets/videos/makeVideo.sh
@@ -0,0 +1,15 @@
+# remove old files
+rm *.png
+rm *.mp4
+rm *.webm
+
+# get new screenshots
+cp ../screenshot/* .
+
+# create video (mp4 format)
+ffmpeg -framerate 0.5 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p screenshots.mp4
+# get webm file
+ffmpeg -i screenshots.mp4 screenshots.webm
+
+# remove screenshots
+rm *.png
