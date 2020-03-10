66e0ac532886eac78d36ebb01067fc8a8bcd7acd
Luca Näf <lucanaef@users.noreply.github.com>
2020-03-10 11:12:12 +0100

    Re-added functioning Fastlane

---
+++ FLSnapshot/FLSnapshot.swift
@@ -0,0 +1,56 @@
+//
+//  FLSnapshot.swift
+//  FLSnapshot
+//
+//  Created by Luca Näf on 10.03.20.
+//  Copyright © 2020 Luca Näf. All rights reserved.
+//
+
+import XCTest
+
+class FLSnapshot: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGenerateScreenshots() {
+        let app = XCUIApplication()
+
+        snapshot("1-EncryptionView")
+
+        app.tabBars.buttons["Decryption"].tap()
+
+        snapshot("2-DecryptionView")
+
+        app.tabBars.buttons["Keychain"].tap()
+
+        snapshot("3-KeychainView")
+
+        app.tables.firstMatch.cells.firstMatch.tap()
+
+        snapshot("4-DetailView")
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
